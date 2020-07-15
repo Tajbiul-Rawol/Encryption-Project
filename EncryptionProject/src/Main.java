b0847d3ea41024199eae0d08889810dcd6ba3120
Rawol <tajbiulrawol@gmail.com>
2020-07-16 00:52:51 +0600

    encryption project

---
+++ EncryptionProject/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        First j1 = new  First();
+        j1.setVisible(true);
+        j1.setDefaultCloseOperation(0);
+        j1.setLocation(120,130);
+    }
+    
+}
